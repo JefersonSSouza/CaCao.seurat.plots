2102a038983955fa8d58b0fdf0900bd30d594cf5
JefersonSSouza <47866049+JefersonSSouza@users.noreply.github.com>
2022-07-13 11:02:32 -0300

    commit

--- R/seuratPlots.R
+++ R/seuratPlots.R
@@ -19,13 +19,15 @@ seurat_barplot <- function(object,ident.use,features_list,
                            show_percentage_legend=T,
                            percentage_legend_size=2,path_to_save=getwd(),width=10, height=14, ncol=1,plot_name='Bar_plot_percentage_features'){
 packages <- c('tidyverse','dplyr','ggplot2','ggpubr','cowplot','gridExtra','RColorBrewer')
-for(pack in packages){
-  if(!require(pack, character.only = TRUE)){
-    install.packages(pack)
+
+package.check <- lapply(packages, FUN = function(x) {
+  if (!require(x, character.only = TRUE)) {
+    install.packages(x, dependencies = TRUE)
   }else{
     library(pack)
   }
-}
+})
+
   require('tidyverse')
   require('dplyr')
   require('ggplot2')
