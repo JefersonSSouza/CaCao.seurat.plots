6fc62fdb24d1776563b3edd8eebee1e7a56b1999
JefersonSSouza <47866049+JefersonSSouza@users.noreply.github.com>
2022-07-13 10:13:08 -0300

    commit

--- R/seuratPlots.R
+++ R/seuratPlots.R
@@ -12,27 +12,21 @@
 #   Install Package:           'Cmd + Shift + B'
 #   Check Package:             'Cmd + Shift + E'
 #   Test Package:              'Cmd + Shift + T'
- require('tidyverse')
-  require('dplyr')
-  require('ggplot2')
-  require('ggpubr')
-  require('cowplot')
-  require('gridExtra')
-  require('RColorBrewer')
-
-seurat_barplot <- function(object,ident.use,features_list,
-                           ident.colors = '',
-                           show_percentage_legend=T,
-                           percentage_legend_size=2,path_to_save=getwd(),width=10, height=14, ncol=1,plot_name='Bar_plot_percentage_features'){
   pacotes = c('tidyverse','dplyr','ggplot2','ggpubr','cowplot','gridExtra','RColorBrewer')
 
   # Run the following command to verify that the required packages are installed. If some package
   # is missing, it will be installed automatically
   package.check <- lapply(pacotes, FUN = function(x) {
     if (!require(x, character.only = TRUE)) {
-      install.packages(x, dependencies = TRUE)
+      remotes::install_local(x, dependencies = TRUE)
     }
   })
+
+seurat_barplot <- function(object,ident.use,features_list,
+                           ident.colors = '',
+                           show_percentage_legend=T,
+                           percentage_legend_size=2,path_to_save=getwd(),width=10, height=14, ncol=1,plot_name='Bar_plot_percentage_features'){
+
   require('tidyverse')
   require('dplyr')
   require('ggplot2')
